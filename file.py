1239703e043bafccbb1f42eada606655be819f3d
stars-art <87599808+stars-art@users.noreply.github.com>
2025-08-20 00:26:43 +0800

    上传文件

--- file.py
+++ file.py
@@ -1,295 +1,140 @@
-# from pickle import dump, load
-# import os
-#
-# def save_training_trainable_output(output):
-#     """
-#     Save the training of the trainable QML model output to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/train_trainable.p", "wb"))
-#     return
-#
-# def load_training_trainable_output():
-#     """
-#     Load the training of the trainable QML model output from file.
-#     """
-#     output = load(open("./data/train_trainable.p", "rb"))
-#     return output
-#
-# def save_training_nonlinear_output(output):
-#     """
-#     Save the training of the trainable QML model output to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/train_nonlinear.p", "wb"))
-#     return
-#
-# def load_training_nonlinear_output():
-#     """
-#     Load the training of the trainable QML model output from file.
-#     """
-#     output = load(open("./data/train_nonlinear.p", "rb"))
-#     return output
-#
-#
-# def save_training_non_trainable_output(output):
-#     """
-#     Save the training of the non trainable QML model output to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/train_non_trainable.p", "wb"))
-#     return
-#
-# def load_training_non_trainable_output():
-#     """
-#     Load the training of the non trainable QML model output from file.
-#     """
-#     output = load(open("./data/train_non_trainable.p", "rb"))
-#     return output
-#
-# def save_trainable_robustness_output(output):
-#     """
-#     Save the robustness output for the trainable encoding QML model to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/robustness_trainable.p", "wb"))
-#     return
-#
-# def load_trainable_robustness_output():
-#     """
-#     Load the robustness output for the trainable encoding QML model from file.
-#     """
-#     output = load(open("./data/robustness_trainable.p", "rb"))
-#     return output
-#
-# def save_nonlinear_robustness_output(output):
-#     """
-#     Save the robustness output for the trainable encoding QML model to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/robustness_nonlinear.p", "wb"))
-#     return
-#
-# def load_nonlinear_robustness_output():
-#     """
-#     Load the robustness output for the trainable encoding QML model from file.
-#     """
-#     output = load(open("./data/robustness_nonlinear.p", "rb"))
-#     return output
-#
-#
-# def save_non_trainable_robustness_output(output):
-#     """
-#     Save the robustness output for the non trainable encoding QML model to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/robustness_non_trainable.p", "wb"))
-#     return
-#
-# def load_non_trainable_robustness_output():
-#     """
-#     Load the robustness output for the non trainable encoding QML model from file.
-#     """
-#     output = load(open("./data/robustness_non_trainable.p", "rb"))
-#     return output
-#
-# def save_trainable_generalization_output(output):
-#     """
-#     Save the generalization output for the trainable encoding QML model to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/generalization_trainable.p", "wb"))
-#     return
-#
-# def load_trainable_generalization_output():
-#     """
-#     Load the generalization output for the trainable encoding QML model from file.
-#     """
-#     output = load(open("./data/generalization_trainable.p", "rb"))
-#     return output
-#
-# def save_nonlinear_generalization_output(output):
-#     """
-#     Save the generalization output for the trainable encoding QML model to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/generalization_nonlinear.p", "wb"))
-#     return
-#
-# def load_nonlinear_generalization_output():
-#     """
-#     Load the generalization output for the trainable encoding QML model from file.
-#     """
-#     output = load(open("./data/generalization_nonlinear.p", "rb"))
-#     return output
-# def save_non_trainable_generalization_output(output):
-#     """
-#     Save the generalization output for the non trainable encoding QML model to file.
-#     """
-#     if not os.path.exists("./data"):
-#         os.makedirs("./data")
-#     dump(output, open("./data/generalization_non_trainable.p", "wb"))
-#     return
-#
-# def load_non_trainable_generalization_output():
-#     """
-#     Load the generalization output for the non trainable encoding QML model from file.
-#     """
-#     output = load(open("./data/generalization_non_trainable.p", "rb"))
-#     return output
-
 from pickle import dump, load
 import os
 
-def save_training_trainable_output(output):
-    """
-    Save the training of the trainable QML model output to file.
-    """
+# 确保数据目录存在
+def ensure_dir():
     if not os.path.exists("./data"):
         os.makedirs("./data")
-    dump(output, open("./data/train_trainable.p", "wb"))
+
+# ==============================
+# NQE-DR 编码方式的文件操作
+# ==============================
+def save_training_nqedr_output(output):
+    """保存NQE-DR编码模型的训练输出"""
+    ensure_dir()
+    dump(output, open("./data/train_nqedr.p", "wb"))
     return
 
-def load_training_trainable_output():
-    """
-    Load the training of the trainable QML model output from file.
-    """
-    output = load(open("./data/train_trainable.p", "rb"))
-    return output
+def load_training_nqedr_output():
+    """加载NQE-DR编码模型的训练输出"""
+    return load(open("./data/train_nqedr.p", "rb"))
 
-def save_training_nonlinear_output(output):
-    """
-    Save the training of the trainable QML model output to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/train_nonlinear.p", "wb"))
+def save_nqedr_robustness_output(output):
+    """保存NQE-DR编码模型的鲁棒性输出"""
+    ensure_dir()
+    dump(output, open("./data/robustness_nqedr.p", "wb"))
     return
 
-def load_training_nonlinear_output():
-    """
-    Load the training of the trainable QML model output from file.
-    """
-    output = load(open("./data/train_nonlinear.p", "rb"))
-    return output
+def load_nqedr_robustness_output():
+    """加载NQE-DR编码模型的鲁棒性输出"""
+    return load(open("./data/robustness_nqedr.p", "rb"))
 
-
-def save_training_non_trainable_output(output):
-    """
-    Save the training of the non trainable QML model output to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/train_non_trainable.p", "wb"))
+def save_nqedr_generalization_output(output):
+    """保存NQE-DR编码模型的泛化能力输出"""
+    ensure_dir()
+    dump(output, open("./data/generalization_nqedr.p", "wb"))
     return
 
-def load_training_non_trainable_output():
-    """
-    Load the training of the non trainable QML model output from file.
-    """
-    output = load(open("./data/train_non_trainable.p", "rb"))
-    return output
+def load_nqedr_generalization_output():
+    """加载NQE-DR编码模型的泛化能力输出"""
+    return load(open("./data/generalization_nqedr.p", "rb"))
 
-def save_trainable_robustness_output(output):
-    """
-    Save the robustness output for the trainable encoding QML model to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/robustness_trainable.p", "wb"))
+# ==============================
+# HEE 编码方式的文件操作
+# ==============================
+def save_training_hee_output(output):
+    """保存HEE编码模型的训练输出"""
+    ensure_dir()
+    dump(output, open("./data/train_hee.p", "wb"))
     return
 
-def load_trainable_robustness_output():
-    """
-    Load the robustness output for the trainable encoding QML model from file.
-    """
-    output = load(open("./data/robustness_trainable.p", "rb"))
-    return output
+def load_training_hee_output():
+    """加载HEE编码模型的训练输出"""
+    return load(open("./data/train_hee.p", "rb"))
 
-def save_nonlinear_robustness_output(output):
-    """
-    Save the robustness output for the trainable encoding QML model to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/robustness_nonlinear.p", "wb"))
+def save_hee_robustness_output(output):
+    """保存HEE编码模型的鲁棒性输出"""
+    ensure_dir()
+    dump(output, open("./data/robustness_hee.p", "wb"))
     return
 
-def load_nonlinear_robustness_output():
-    """
-    Load the robustness output for the trainable encoding QML model from file.
-    """
-    output = load(open("./data/robustness_nonlinear.p", "rb"))
-    return output
+def load_hee_robustness_output():
+    """加载HEE编码模型的鲁棒性输出"""
+    return load(open("./data/robustness_hee.p", "rb"))
 
-
-def save_non_trainable_robustness_output(output):
-    """
-    Save the robustness output for the non trainable encoding QML model to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/robustness_non_trainable.p", "wb"))
+def save_hee_generalization_output(output):
+    """保存HEE编码模型的泛化能力输出"""
+    ensure_dir()
+    dump(output, open("./data/generalization_hee.p", "wb"))
     return
 
-def load_non_trainable_robustness_output():
-    """
-    Load the robustness output for the non trainable encoding QML model from file.
-    """
-    output = load(open("./data/robustness_non_trainable.p", "rb"))
-    return output
+def load_hee_generalization_output():
+    """加载HEE编码模型的泛化能力输出"""
+    return load(open("./data/generalization_hee.p", "rb"))
 
-def save_trainable_generalization_output(output):
-    """
-    Save the generalization output for the trainable encoding QML model to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/generalization_trainable.p", "wb"))
+# ==============================
+# AE 编码方式的文件操作
+# ==============================
+def save_training_ae_output(output):
+    """保存AE编码模型的训练输出"""
+    ensure_dir()
+    dump(output, open("./data/train_ae.p", "wb"))
     return
 
-def load_trainable_generalization_output():
-    """
-    Load the generalization output for the trainable encoding QML model from file.
-    """
-    output = load(open("./data/generalization_trainable.p", "rb"))
-    return output
+def load_training_ae_output():
+    """加载AE编码模型的训练输出"""
+    return load(open("./data/train_ae.p", "rb"))
 
-def save_nonlinear_generalization_output(output):
-    """
-    Save the generalization output for the trainable encoding QML model to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/generalization_nonlinear.p", "wb"))
+def save_ae_robustness_output(output):
+    """保存AE编码模型的鲁棒性输出"""
+    ensure_dir()
+    dump(output, open("./data/robustness_ae.p", "wb"))
     return
 
-def load_nonlinear_generalization_output():
-    """
-    Load the generalization output for the trainable encoding QML model from file.
-    """
-    output = load(open("./data/generalization_nonlinear.p", "rb"))
-    return output
-def save_non_trainable_generalization_output(output):
-    """
-    Save the generalization output for the non trainable encoding QML model to file.
-    """
-    if not os.path.exists("./data"):
-        os.makedirs("./data")
-    dump(output, open("./data/generalization_non_trainable.p", "wb"))
+def load_ae_robustness_output():
+    """加载AE编码模型的鲁棒性输出"""
+    return load(open("./data/robustness_ae.p", "rb"))
+
+def save_ae_generalization_output(output):
+    """保存AE编码模型的泛化能力输出"""
+    ensure_dir()
+    dump(output, open("./data/generalization_ae.p", "wb"))
     return
 
-def load_non_trainable_generalization_output():
-    """
-    Load the generalization output for the non trainable encoding QML model from file.
-    """
-    output = load(open("./data/generalization_non_trainable.p", "rb"))
-    return output
+def load_ae_generalization_output():
+    """加载AE编码模型的泛化能力输出"""
+    return load(open("./data/generalization_ae.p", "rb"))
+
+# ==============================
+# AmE 编码方式的文件操作
+# ==============================
+def save_training_ame_output(output):
+    """保存AmE编码模型的训练输出"""
+    ensure_dir()
+    dump(output, open("./data/train_ame.p", "wb"))
+    return
+
+def load_training_ame_output():
+    """加载AmE编码模型的训练输出"""
+    return load(open("./data/train_ame.p", "rb"))
+
+def save_ame_robustness_output(output):
+    """保存AmE编码模型的鲁棒性输出"""
+    ensure_dir()
+    dump(output, open("./data/robustness_ame.p", "wb"))
+    return
+
+def load_ame_robustness_output():
+    """加载AmE编码模型的鲁棒性输出"""
+    return load(open("./data/robustness_ame.p", "rb"))
+
+def save_ame_generalization_output(output):
+    """保存AmE编码模型的泛化能力输出"""
+    ensure_dir()
+    dump(output, open("./data/generalization_ame.p", "wb"))
+    return
+
+def load_ame_generalization_output():
+    """加载AmE编码模型的泛化能力输出"""
+    return load(open("./data/generalization_ame.p", "rb"))
+
